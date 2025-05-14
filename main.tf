5c5b9362ff188c9b9fdd606dfe83a0e56c3259bf
Douglas Oliveira <douglas.oliveira@esportudo.com>
2025-05-14 10:02:05 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_db_instance" "default" {
+  identifier = var.db_identifier
+  engine = "mysql"
+  engine_version = "8.0"
+  instance_class = "db.t3.micro"
+  allocated_storage = 20
+  max_allocated_storage = 100
+  db_name = var.db_name
+  username = var.db_username
+  password = var.db_password
+  publicly_accessible = true
+  skip_final_snapshot = true
+  backup_retention_period = 0
+  multi_az = false
+  deletion_protection = true
+  storage_type = "gp2"
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "db_identifier" {
+  type = string
+  description = "Identificador único do RDS"
+}
+
+variable "db_name" {
+  type = string
+  description = "Nome do DB"
+}
+
+variable "db_username" {
+  type = string
+  description = "DB User"
+  sensitive = true
+}
+
+variable "db_password" {
+  type = string
+  description = "DB Password"
+  sensitive = true
+}
