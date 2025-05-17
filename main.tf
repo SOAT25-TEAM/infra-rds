5cd3c4d8911d4d8a032d047f103c925cbdb53471
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-17 11:13:15 -0300

    adjust RDS 2

--- main.tf
+++ main.tf
@@ -9,57 +9,72 @@ resource "aws_vpc" "soat_vpc" {
   }
 }
 
-resource "aws_subnet" "public_subnet_soat" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.soat_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"  
-  map_public_ip_on_launch = true  
+  availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = true
   tags = {
-    Name = "subnet-publica-soat"
+    Name = "subnet-publica-1"
   }
 }
 
-resource "aws_internet_gateway" "soat_igw" {
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.soat_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet-publica-2"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.soat_vpc.id
   tags = {
     Name = "igw-soat"
   }
 }
 
-resource "aws_route_table" "soat_public_route" {
+resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.soat_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.soat_igw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "rota-publica-soat"
+    Name = "rota-publica"
   }
 }
 
-resource "aws_route_table_association" "soat_public_assoc" {
-  subnet_id      = aws_subnet.public_subnet_soat.id
-  route_table_id = aws_route_table.soat_public_route.id
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route.id
 }
 
-resource "aws_db_subnet_group" "soat_rds_subnet_group" {
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group-soat"
-  subnet_ids = [aws_subnet.public_subnet_soat.id]
+  subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   tags = {
-    Name = "subnet-group-rds-soat"
+    Name = "subnet-group-rds"
   }
 }
 
-resource "aws_security_group" "soat_rds_open" {
-  name        = "rds-acesso-total-soat"
-  description = "Libera TODAS as portas (uso exclusivo para SOAT)"
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-mysql-access"
+  description = "Libera MySQL para o SOAT"
   vpc_id      = aws_vpc.soat_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -72,29 +87,29 @@ resource "aws_security_group" "soat_rds_open" {
   }
 
   tags = {
-    Name = "sg-rds-soat"
+    Name = "sg-rds-mysql"
   }
 }
 
-resource "aws_db_instance" "soat_db" {
-  identifier           = "db-soat-mysql"
+resource "aws_db_instance" "default" {
+  identifier           = var.db_identifier
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   allocated_storage    = 20
   max_allocated_storage = 100
-  db_name              = "soatdb"
-  username             = "soatadmin"
-  password             = "soatpassword123"  
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
   publicly_accessible  = true
   skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.soat_rds_open.id]
-  db_subnet_group_name = aws_db_subnet_group.soat_rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
   tags = {
     Name = "mysql-soat"
   }
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.soat_db.endpoint
+  value = aws_db_instance.default.endpoint
 }
