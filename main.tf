be6987af01f4b99d22efe9f063d04e39085c68c8
douglas.oliveira <douglasoliveira320@gmail.com>
2025-05-17 11:23:40 -0300

    adjust RDS 3

--- main.tf
+++ main.tf
@@ -3,26 +3,28 @@ provider "aws" {
 }
 
 resource "aws_vpc" "soat_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true  
+  enable_dns_hostnames = true  
   tags = {
     Name = "vpc-soat"
   }
 }
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.soat_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.soat_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true  
   tags = {
     Name = "subnet-publica-1"
   }
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.soat_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "${var.aws_region}b"
+  vpc_id                  = aws_vpc.soat_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
   map_public_ip_on_launch = true
   tags = {
     Name = "subnet-publica-2"
@@ -38,12 +40,10 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_route_table" "public_route" {
   vpc_id = aws_vpc.soat_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
   tags = {
     Name = "rota-publica"
   }
@@ -76,7 +76,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   egress {
@@ -92,19 +92,20 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_db_instance" "default" {
-  identifier           = var.db_identifier
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  allocated_storage    = 20
-  max_allocated_storage = 100
-  db_name              = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  publicly_accessible  = true
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  identifier              = var.db_identifier
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  max_allocated_storage   = 100
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  publicly_accessible     = true  
+  skip_final_snapshot     = true
+  backup_retention_period = 0
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   tags = {
     Name = "mysql-soat"
   }
